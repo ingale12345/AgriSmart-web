d1b517b06da0d7846f830cac114df6696d16dbfc
ingale12345 <ingaleprajawal@.com>
2025-05-28 16:10:32 +0530

    added routes dir for route specific childs and configurations

---
+++ src/routes/agri-smart.tsx
@@ -0,0 +1,40 @@
+import About from "@/pages/About";
+import Branches from "@/pages/Branches";
+import Crops from "@/pages/Crops";
+import CustomerCrops from "@/pages/CustomerCrops";
+import Deliveries from "@/pages/Deliveries";
+import GeoTags from "@/pages/GeoTags";
+import Home from "@/pages/Home";
+import InventoryItems from "@/pages/InventoryItems";
+import Invoices from "@/pages/Invoices";
+import Orders from "@/pages/Orders";
+import ProductApplications from "@/pages/ProductApplications";
+import { Shops } from "@/pages/shops";
+import ShopUsers from "@/pages/ShopUsers";
+import Users from "@/pages/Users";
+
+export const agriSmartRoutes = [
+  { index: true, element: <Home />, title: "Dashboard" },
+  { path: "home", element: <Home />, title: "Dashboard" },
+  { path: "about", element: <About />, isWrapped: false },
+  { path: "shops", element: <Shops />, title: "Shops" },
+  { path: "branches", element: <Branches />, title: "Branches" },
+  { path: "shop-users", element: <ShopUsers />, title: "Shop Users" },
+  { path: "inventory", element: <InventoryItems />, title: "Inventory" },
+  { path: "orders", element: <Orders />, title: "Orders" },
+  { path: "invoices", element: <Invoices />, title: "Invoices" },
+  { path: "deliveries", element: <Deliveries />, title: "Deliveries" },
+  { path: "crops", element: <Crops />, title: "Crops" },
+  {
+    path: "customer-crops",
+    element: <CustomerCrops />,
+    title: "Customer Crops",
+  },
+  {
+    path: "product-applications",
+    element: <ProductApplications />,
+    title: "Product Applications",
+  },
+  { path: "geo-tags", element: <GeoTags />, title: "Geo Tags" },
+  { path: "users", element: <Users />, title: "Users" },
+];
