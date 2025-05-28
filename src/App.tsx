a8cd3df797b9b33376095937b45bf2bbf3fa4995
ingale12345 <ingaleprajawal@.com>
2025-05-28 14:02:32 +0530

    added changes in router for protected route

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { ThemeProvider } from "./components/theme-provider";
-import Shops from "./pages/Shops";
+import { Shops } from "./pages/shops";
 import Branches from "./pages/Branches";
 import ShopUsers from "./pages/ShopUsers";
 import InventoryItems from "./pages/InventoryItems";
@@ -19,6 +19,7 @@ import Users from "./pages/Users";
 import Login from "./pages/Login";
 import PageWrapper from "./components/PageWrapper";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import ProtectedRoute from "./ProtectedRoute";
 function App() {
   const queryClient = new QueryClient();
   const router = createBrowserRouter([
@@ -29,79 +30,85 @@ function App() {
     {
       path: "agri-smart/",
       // element: <Login />,
-      element: <MainLayout />,
+      // element: <MainLayout />,
+      element: <ProtectedRoute />,
       children: [
         {
-          index: true,
-          element: (
-            <PageWrapper title="Dashboard">
-              <Home />
-            </PageWrapper>
-          ),
-        },
-        {
-          path: "home",
-          element: (
-            <PageWrapper title="Dashboard">
-              <Home />
-            </PageWrapper>
-          ),
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "shops",
-          element: (
-            <PageWrapper title="Shops">
-              <Shops />
-            </PageWrapper>
-          ),
-        },
-        {
-          path: "branches",
-          element: <Branches />,
-        },
-        {
-          path: "shop-users",
-          element: <ShopUsers />,
-        },
-        {
-          path: "inventory",
-          element: <InventoryItems />,
-        },
-        {
-          path: "orders",
-          element: <Orders />,
-        },
-        {
-          path: "invoices",
-          element: <Invoices />,
-        },
-        {
-          path: "deliveries",
-          element: <Deliveries />,
-        },
-        {
-          path: "crops",
-          element: <Crops />,
-        },
-        {
-          path: "customer-crops",
-          element: <CustomerCrops />,
-        },
-        {
-          path: "product-applications",
-          element: <ProductApplicaions />,
-        },
-        {
-          path: "geo-tags",
-          element: <GeoTags />,
-        },
-        {
-          path: "users",
-          element: <Users />,
+          element: <MainLayout />,
+          children: [
+            {
+              index: true,
+              element: (
+                <PageWrapper title="Dashboard">
+                  <Home />
+                </PageWrapper>
+              ),
+            },
+            {
+              path: "home",
+              element: (
+                <PageWrapper title="Dashboard">
+                  <Home />
+                </PageWrapper>
+              ),
+            },
+            {
+              path: "about",
+              element: <About />,
+            },
+            {
+              path: "shops",
+              element: (
+                <PageWrapper title="Shops">
+                  <Shops />
+                </PageWrapper>
+              ),
+            },
+            {
+              path: "branches",
+              element: <Branches />,
+            },
+            {
+              path: "shop-users",
+              element: <ShopUsers />,
+            },
+            {
+              path: "inventory",
+              element: <InventoryItems />,
+            },
+            {
+              path: "orders",
+              element: <Orders />,
+            },
+            {
+              path: "invoices",
+              element: <Invoices />,
+            },
+            {
+              path: "deliveries",
+              element: <Deliveries />,
+            },
+            {
+              path: "crops",
+              element: <Crops />,
+            },
+            {
+              path: "customer-crops",
+              element: <CustomerCrops />,
+            },
+            {
+              path: "product-applications",
+              element: <ProductApplicaions />,
+            },
+            {
+              path: "geo-tags",
+              element: <GeoTags />,
+            },
+            {
+              path: "users",
+              element: <Users />,
+            },
+          ],
         },
       ],
     },
