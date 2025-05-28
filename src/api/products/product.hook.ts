3a4d790f11a5e3e3b3b6795aaa2d845ab0c2da4d
ingale12345 <ingaleprajawal@.com>
2025-05-28 14:05:04 +0530

    files name changes

--- src/api/products/product.hook.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchProducts } from "./products.api";
-
-export const useGetProducts = () => {
-  return useQuery({ queryKey: ["get-products"], queryFn: fetchProducts });
-};
