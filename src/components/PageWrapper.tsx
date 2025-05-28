3dee7837fea59b710635da3f2cec8ea62a60e3cb
ingale12345 <ingaleprajawal@.com>
2025-05-28 15:17:00 +0530

    added page wrapper chnegs

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -7,28 +7,26 @@ export type PageSectionProps = {
   titleClassName?: string;
 };
 
-export default function PageWrapper({
+const PageWrapper = ({
   title,
   children,
   className = "",
   titleClassName = "",
-}: PageSectionProps) {
-  console.log("className is ", className);
-  return (
-    <div className={cn("p-6", className)}>
-      {typeof title === "string" ? (
-        <span
-          className={cn(
-            "flex mb-6 text-3xl font-bold tracking-tight w-full  text-start",
-            titleClassName
-          )}
-        >
-          {title}
-        </span>
-      ) : (
-        title
-      )}
-      {children}
-    </div>
-  );
-}
+}: PageSectionProps) => (
+  <div className={cn("p-6", className)}>
+    {typeof title === "string" ? (
+      <span
+        className={cn(
+          "flex mb-6 text-3xl font-bold tracking-tight w-full  text-start",
+          titleClassName
+        )}
+      >
+        {title}
+      </span>
+    ) : (
+      title
+    )}
+    {children}
+  </div>
+);
+export default PageWrapper;
